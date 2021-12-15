54af61de183e8c643fd0db075504c036efe9b721
Muhammad Aminudin <aminudin199809@gmail.com>
2021-12-15 23:57:16 +0700

    Tugas 9 js
    Mohon untuk direview kembali

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function tugas9() {
+  var pemain = {
+    psg: "messi",
+    madrid: "benzema",
+    mu: "ronaldo",
+    liv: "salah",
+  };
+
+  for (var i in pemain) {
+    console.log(pemain[i]);
+  }
+}
+tugas9();
